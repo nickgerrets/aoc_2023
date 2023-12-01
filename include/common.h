968e573196bc2b403a4f6edc54e6d5c6509f754d
Nick <nickgerrets@gmail.com>
2023-12-01 07:06:07 +0100

    formatting

--- include/common.h
+++ include/common.h
@@ -6,23 +6,23 @@
 
 # define SIGN(x) (x > 0) - (x < 0)
 
-namespace aoc
-{
+namespace aoc {
 
 //	ignore stream until next digit
-std::istream& next_digit(std::istream& in)
-{
+std::istream& next_digit(std::istream& in) {
 	char c;
-	while (in && (c = (in >> std::ws).peek()) != EOF
-		&& !(c >= '0' && c <= '9') && c != '-')
+	while (in &&
+		   (c = (in >> std::ws).peek()) != EOF &&
+		   !(c >= '0' && c <= '9') &&
+		   c != '-') {
 		in.ignore();
+	}
 	return (in);
 }
 
 //	skips until (and including) next CHARACTER
 template <char CHARACTER>
-std::istream& skip(std::istream& in)
-{
+std::istream& skip(std::istream& in) {
 	return (in.ignore(std::numeric_limits<std::streamsize>::max(), CHARACTER));
 }
 
