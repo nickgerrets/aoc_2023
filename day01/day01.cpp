d2ee9e17f12474d0a4a36880f0d5f9d8d2e9eb1c
Nick <nickgerrets@gmail.com>
2023-12-01 07:00:02 +0100

    day 1

---
+++ day01/day01.cpp
@@ -0,0 +1,100 @@
+#include "common.h"
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
+
+namespace aoc {
+
+// This map will help connect a value to the words and will also provide
+// a handy 'list' to iterate through
+static std::unordered_map<std::string, uint64_t> digit_map {
+	{"zero", 0},
+	{"one", 1},	
+	{"two", 2},	
+	{"three", 3},	
+	{"four", 4},	
+	{"five", 5},	
+	{"six", 6},	
+	{"seven", 7},	
+	{"eight", 8},	
+	{"nine", 9},	
+};
+
+uint64_t get_first_digit(std::string const& line) {
+	uint64_t digit = 0;
+	size_t pos = std::string::npos;
+
+	// first occurence of an actual digit
+	for (size_t i = 0; i < line.length(); ++i) {
+		if (std::isdigit(line[i])) {
+			digit = line[i] - '0';
+			pos = i;
+			break ;
+		}
+	}
+
+	// first occurence of a word that represents a digit
+	// taking into account it needs to have a lower position than the digit did
+	for (auto const& pair : digit_map) {
+		size_t found = line.find(pair.first);
+		if (found < pos) {
+			pos = found;
+			digit = pair.second;
+		}
+	}
+	return digit;
+}
+
+uint64_t get_last_digit(std::string const& line) {
+	// Very similar to get_first_digit, just reversed
+	uint64_t digit = 0;
+	size_t pos = 0;
+
+	// the length + 1 and [i - 1] are to make sure we don't have to make the conditon 'i >= 0'
+	// which would cause an underflow and an infinite loop
+	for (size_t i = line.length() + 1; i > 0; --i) {
+		if (std::isdigit(line[i - 1])) {
+			digit = line[i - 1] - '0';
+			pos = i - 1;
+			break ;
+		}
+	}
+
+	// simply use rfind instread of find and '>' instead of '<'
+	// did have to specifically exclude npos, because that's always higher and returned on 'not found'
+	for (auto const& pair : digit_map) {
+		size_t found = line.rfind(pair.first);
+		if (found != std::string::npos && found > pos) {
+			pos = found;
+			digit = pair.second;
+		}
+	}
+	return digit;
+}
+
+uint64_t get_sum_first_and_last(std::ifstream& stream) {
+	uint64_t sum = 0;
+	// Iterate through the entire document line by line
+	for (std::string line; std::getline(stream, line); ) {
+		sum += get_first_digit(line) * 10 + get_last_digit(line);
+	}
+
+	return sum;
+}
+
+} // aoc
+
+int	main(int argc, char **argv) {
+	if (argc < 2)
+		return (EXIT_FAILURE);
+
+	std::ifstream file(argv[1]);
+	if (!file)
+		return (EXIT_FAILURE);
+	
+	uint64_t sum = aoc::get_sum_first_and_last(file);
+
+	std::cout << "Sum of all calibration values: " << sum << std::endl;
+	
+	return (EXIT_SUCCESS);
+}
---
+++ include/common.h
@@ -0,0 +1,31 @@
+#ifndef COMMON_H
+# define COMMON_H
+
+# include <istream>
+# include <limits>
+
+# define SIGN(x) (x > 0) - (x < 0)
+
+namespace aoc
+{
+
+//	ignore stream until next digit
+std::istream& next_digit(std::istream& in)
+{
+	char c;
+	while (in && (c = (in >> std::ws).peek()) != EOF
+		&& !(c >= '0' && c <= '9') && c != '-')
+		in.ignore();
+	return (in);
+}
+
+//	skips until (and including) next CHARACTER
+template <char CHARACTER>
+std::istream& skip(std::istream& in)
+{
+	return (in.ignore(std::numeric_limits<std::streamsize>::max(), CHARACTER));
+}
+
+} // namespace aoc
+
+#endif // COMMON_H
