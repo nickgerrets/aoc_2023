4156e9d0e5c89e9854f35cee25b056bdadbcf445
Nick <nickgerrets@gmail.com>
2023-12-04 07:00:10 +0100

    solution day 4

---
+++ day04/scratchcard.cpp
@@ -0,0 +1,94 @@
+#include "common.h"
+
+#include <set>
+#include <vector>
+#include <sstream>
+
+struct Scratchcard {
+	uint64_t amount;
+
+	// Arrays (vectors) would've probably done just as well,
+	// but sets make it easier to match and the numbers are unique
+	std::set<uint64_t> owned;
+	std::set<uint64_t> winning;
+
+	uint64_t calculate_matches() const {
+		uint64_t matches = 0;
+		// Simply check the owned set against the winning set to get your matches
+		for (uint64_t const& o : owned) {
+			matches += winning.count(o);
+		}
+		return matches;
+	}
+
+	uint64_t calculate_value() const {
+		// bitshift power-of-two
+		return (uint64_t(1) << calculate_matches()) >> 1;
+	}
+};
+
+std::vector<Scratchcard> parse_cards(std::istream& stream) {
+	std::vector<Scratchcard> cards;
+	for (auto& line : aoc::Lines(stream)) {
+		std::istringstream ss(line);
+
+		Scratchcard card;
+		card.amount = 1;
+
+		std::string word;
+		// skip "Card x"
+		ss >> aoc::next_digit >> word;
+
+		while (ss >> word && word[0] != '|') {
+			card.winning.emplace(std::stoull(word));
+		}
+		while (ss >> word) {
+			card.owned.emplace(std::stoull(word));
+		}
+		cards.emplace_back(std::move(card));
+	}
+	return cards;
+}
+
+void process_copies(std::vector<Scratchcard>& cards) {
+	for (size_t i = 0; i < cards.size(); ++i) {
+		// The way you process how many copies you get is quite simple.
+		// You just add your own card amount to the amount of the next x cards
+		// where x is how many numbers match.
+		uint64_t matches = cards[i].calculate_matches();
+		for (size_t j = i + 1; j < cards.size() && j <= i + matches; ++j) {
+			cards[j].amount = cards[j].amount + cards[i].amount;
+		}
+	}
+}
+
+uint64_t sum_card_amounts(std::vector<Scratchcard> const& cards) {
+	uint64_t sum = 0;
+	for (auto& card : cards) {
+		sum += card.amount;
+	}
+	return sum;
+}
+
+uint64_t sum_card_values(std::vector<Scratchcard> const& cards) {
+	uint64_t sum = 0;
+	for (auto& card : cards) {
+		sum += card.calculate_value();
+	}
+	return sum;
+}
+
+int main(int argc, char** argv) {
+	auto file = aoc::get_input_file(argc, argv);
+
+	// While for part 1 you really don't need to store the parsed cards,
+	// part 2 becomes a lot easier if you do.
+	std::vector<Scratchcard> cards = parse_cards(file);
+
+	std::cout << "(Part 1) Sum of scratchcard values:  " << sum_card_values(cards) << std::endl;
+
+	process_copies(cards);
+	std::cout << "(Part 2) Sum of scratchcard amounts: " << sum_card_amounts(cards) << std::endl;
+
+	return (EXIT_SUCCESS);
+}
